8ad6c46146725235f55ea5f6fd18ebe1f6607652
huynguyen8870 <55775186+huynguyen8870@users.noreply.github.com>
2021-04-25 09:59:01 +0700

    post bill MV

---
+++ Recharge_Mobile/Models/Views/PostPaymentModelView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Recharge_Mobile.Models.Views
+{
+    public class PostPaymentModelView
+    {
+        public PostPaymentModelView()
+        {
+                
+        }
+
+        public int TransactionId { get; set; }
+        public DateTime DateTime { get; set; }
+        public decimal Amount { get; set; }
+        public string Status { get; set; }
+        
+    }
+}
